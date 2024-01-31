f8dca2937ea701af9749dc28d006c7595ae908e5
Cassy <119490174+cassy25@users.noreply.github.com>
2024-01-31 15:51:57 +0100

    Add files via upload

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,48 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <string>
+#include <stack>
+#include <list>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+    public:
+        MutantStack() : std::stack<T>(){}
+        ~MutantStack(){}
+        MutantStack(MutantStack const& copy)
+        {
+            *this = copy;
+        }
+        MutantStack& operator=(MutantStack const& copy)
+        {
+            (void)copy;
+            return (*this);
+        }
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        iterator begin() {return this->c.begin();}
+        iterator end() {return this->c.end();}
+};
+
+/*template <typename T, typename Container = std::list<T> >
+class MutantStack : public Container
+{
+    public:
+        MutantStack() : Container() {}
+        ~MutantStack(){}
+        MutantStack(MutantStack const& copy)
+        {
+            *this = copy;
+        }
+        MutantStack& operator=(MutantStack const& copy)
+        {
+            return (*this);
+        }
+        typedef typename Container::iterator iterator;
+        iterator begin() {return Container::begin();}
+        iterator end() {return Container::end();}
+};*/
+
+#endif
